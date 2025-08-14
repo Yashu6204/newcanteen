02eaa92afc159283221d0ff2dc89ae2fe50f3c93
Yashu6204 <yesaswisimhadri2006@gmail.com>
2025-08-14 17:44:27 +0530

    Updated AdminPanel.tsx

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -19,7 +19,7 @@ export const AdminPanel: React.FC<AdminPanelProps> = ({ menuItems, onUpdateMenu 
     available: true,
   });
 
-  const categories = ['breakfast', 'main-course', 'soft-drinks', 'fast-foods', 'beverages', 'bakery','ice-creams'];
+  const categories = ['breakfast', 'main-course', 'soft-drinks', 'fast-foods', 'beverages', 'bakery','ice-cream'];
 
   const toggleAvailability = (id: string) => {
     const updatedItems = items.map(item =>
